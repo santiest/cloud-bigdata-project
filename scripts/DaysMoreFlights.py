9d7273c1730b93e41d58fc162b332b8e417b61e5
aalvaaro99 <alvlop14@ucm.es>
2022-12-06 12:50:51 +0100

    DaysMoreFlights

---
+++ scripts/DaysMoreFlights.py
@@ -0,0 +1,25 @@
+
+import sys
+sys.path.insert(0,'..')
+from env_wrapper import EnvVariables
+
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
+
+env = EnvVariables()
+print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
+
+# Build pyspark
+spark = (SparkSession.builder.master("local[4]")
+            .appName("GetDaysMoreFlights")
+            .getOrCreate())
+
+df = spark.read.option("header",True).csv(env.getFileName())
+
+# Average values
+result_df = df.groupBy(col("flightDate")).count()
+
+# Write to file
+result_df.sort("flightDate").write.option("header",True).csv("output")
+
+# spark-submit DaysMoreFlights.py
