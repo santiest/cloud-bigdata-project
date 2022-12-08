5090efc0b5d23ccb17a46d312237a31526a41367
aalvaaro99 <alvlop14@ucm.es>
2022-12-08 14:06:23 +0100

    RecomendacionesPrecios

---
+++ scripts/BestFlightsCitiesRefundable.py
@@ -0,0 +1,26 @@
+
+import sys
+sys.path.insert(0,'..')
+from env_wrapper import EnvVariables
+
+from pyspark.sql.functions import col, bround, ceil
+from pyspark.sql import SparkSession
+
+env = EnvVariables()
+print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
+
+# Build pyspark
+spark = (SparkSession.builder.master("local[4]")
+            .appName("BestFlightsCitiesRefundable")
+            .getOrCreate()) 
+
+df = spark.read.option("header",True).csv(env.getFileName())
+
+# Average values
+result_df = df.filter(df.isRefundable == "True")
+result_df = df.withColumn("totalFare", col("totalFare").cast("float")).groupBy("startingAirport","destinationAirport").min("totalFare")
+
+# Write to file
+result_df.sort("startingAirport").write.option("header",True).csv("BestFlightsCitiesRefundable")
+
+# spark-submit BestFlightsCitiesRefundable.py
