67ceaca177a3b509ec8fc7fcde341701d1663104
aalvaaro99 <alvlop14@ucm.es>
2022-12-08 14:09:00 +0100

    recomendaciones 2

---
+++ scripts/BestFlightsCitiesEconomyNonStop.py
@@ -0,0 +1,26 @@
+
+import sys
+sys.path.insert(0,'..')
+from env_wrapper import EnvVariables
+
+from pyspark.sql.functions import col, bround, ceil
+from pyspark.sql import SparkSession
+
+env = EnvVariables()
+print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
+
+# Build pyspark
+spark = (SparkSession.builder.master("local[4]")
+            .appName("BestFlightsCitiesEconomyNonStop")
+            .getOrCreate()) 
+
+df = spark.read.option("header",True).csv(env.getFileName())
+
+# Average values
+result_df = df.filter((df.isNonStop == "True") & (df.isBasicEconomy == "True"))
+result_df = df.withColumn("totalFare", col("totalFare").cast("float")).groupBy("startingAirport","destinationAirport").min("totalFare")
+
+# Write to file
+result_df.sort("startingAirport").write.option("header",True).csv("BestFlightsCitiesEconomyNonStop")
+
+# spark-submit BestFlightsCitiesEconomyNonStop.py
---
+++ scripts/WorstFlightsCitiesNonStop.py
@@ -0,0 +1,26 @@
+
+import sys
+sys.path.insert(0,'..')
+from env_wrapper import EnvVariables
+
+from pyspark.sql.functions import col, bround, ceil
+from pyspark.sql import SparkSession
+
+env = EnvVariables()
+print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
+
+# Build pyspark
+spark = (SparkSession.builder.master("local[4]")
+            .appName("WorstFlightsCitiesNonStop")
+            .getOrCreate()) 
+
+df = spark.read.option("header",True).csv(env.getFileName())
+
+# Average values
+result_df = df.filter(df.isNonStop == "True")
+result_df = df.withColumn("totalFare", col("totalFare").cast("float")).groupBy("startingAirport","destinationAirport").max("totalFare")
+
+# Write to file
+result_df.sort("startingAirport").write.option("header",True).csv("WorstFlightsCitiesNonStop")
+
+# spark-submit WorstFlightsCitiesNonStop.py
