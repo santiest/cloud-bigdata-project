d6124017d550ed93f5855b61c37a6e62b02cb184
lydigmz <lysapema@gmail.com>
2022-12-11 16:32:26 +0100

    Fixes FlightPerMonth and SeatsRemaining

--- scripts/FlightsPerMonth.py
+++ scripts/FlightsPerMonth.py
@@ -1,20 +1,26 @@
 # Este programa calcula el numero de vuelos de cada aerolinea para un destino y origen pedido por cada mes. 
 
+
 import sys
 sys.path.insert(0,'..')
 from env_wrapper import EnvVariables
 from schema import DatasetSchema
+from pyspark import SparkFiles
 
 from pyspark.sql.functions import col, split, floor
 from pyspark.sql import SparkSession
 
+# Build pyspark
+spark = (SparkSession.builder.master("local[*]")
+            .appName("FlightsPerMonth")
+            .getOrCreate())
+
+spark.sparkContext.addPyFile(SparkFiles.get("env_wrapper.py"))
+spark.sparkContext.addPyFile(SparkFiles.get("schema.py"))
+
 env = EnvVariables()
 print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
 
-# Build pyspark
-spark = (SparkSession.builder.master("local[4]")
-            .appName("FlightsPerMonth")
-            .getOrCreate())
 
 df = spark.read.option("header",True).schema(DatasetSchema().schema).csv(env.getFileName())
 
@@ -32,6 +38,6 @@ result_df1 = result_df1.withColumn('Month', split('flightDate', '-').getItem(1))
 result_df2 = result_df1.groupBy('Month', 'Airline').count()
 
 # Ordeno los datos y los escribo en un fichero
-result_df2.sort('Airline', 'Month').write.option("header",True).csv("FlightsPerMonth")
+result_df2.sort('Airline', 'Month').write.option("header",True).mode("overwrite").csv(env.getOutputDir() + spark.sparkContext.appName)
 
 # spark-submit FlightsPerMonth.py
--- scripts/SeatsRemainingPerHour.py
+++ scripts/SeatsRemainingPerHour.py
@@ -5,17 +5,23 @@ import sys
 sys.path.insert(0,'..')
 from env_wrapper import EnvVariables
 from schema import DatasetSchema
+from pyspark import SparkFiles
 
 from pyspark.sql.functions import col, split, floor
 from pyspark.sql import SparkSession
 
+# Build pyspark
+spark = (SparkSession.builder.master("local[*]")
+            .appName("SeatsRemainingPerHour")
+            .getOrCreate())
+
+spark.sparkContext.addPyFile(SparkFiles.get("env_wrapper.py"))
+spark.sparkContext.addPyFile(SparkFiles.get("schema.py"))
+
 env = EnvVariables()
 print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
 
-# Build pyspark
-spark = (SparkSession.builder.master("local[4]")
-            .appName("SeatsRemainingPerHour")
-            .getOrCreate())
+
 
 df = spark.read.option("header",True).schema(DatasetSchema().schema).csv(env.getFileName())
 
@@ -28,14 +34,13 @@ result_df1 = result_df1.withColumn('Airline', split(df.segmentsAirlineName, '\|'
 
 # Saco la hora de partida,  YYYY-MM-DDThh:mm:ss.000±[hh]:00
 result_df2 = result_df1.withColumn('auxhour', split(result_df1.segmentsDepartureTimeRaw, 'T').getItem(1))
-result_df3 = result_df2.withColumn('Hour', split(result_df2.auxhour, ':').getItem(0))\
-                       .drop('auxhour','segmentsDepartureTimeRaw', 'segmentsAirlineName')
+result_df3 = result_df2.withColumn('Hour', split(result_df2.auxhour, ':').getItem(0)).drop('auxhour','segmentsDepartureTimeRaw', 'segmentsAirlineName')
 
 # Calculo la media de los sitios que sobran por cada hora los redondeo
 result_df3 = result_df3.groupBy('Airline', 'Hour').avg('seatsRemaining')
 result_df3 = result_df3.select('Airline','Hour', floor("avg(seatsRemaining)").alias('avgSeats'))
 
 # Ordeno los datos y los escribo en un fichero
-result_df3.sort('Airline', 'Hour').write.option("header",True).csv("SeatsRemainingPerHour")
+result_df3.sort('Airline', 'Hour').write.option("header",True).mode("overwrite").csv(env.getOutputDir() + spark.sparkContext.appName)
 
 # spark-submit SeatsRemainingPerHour.py
