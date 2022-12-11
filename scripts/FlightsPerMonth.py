2159b62cb33d567807f1a277ba006e0d9c24ce23
lydigmz <lysapema@gmail.com>
2022-12-11 13:05:09 +0100

    Con aerolines

--- scripts/FlightsPerMonth.py
+++ scripts/FlightsPerMonth.py
@@ -1,4 +1,4 @@
-# Este programa calcula el numero de vuelos con el mismo destino y origen pedido por cada mes. 
+# Este programa calcula el numero de vuelos de cada aerolinea para un destino y origen pedido por cada mes. 
 
 import sys
 sys.path.insert(0,'..')
@@ -19,15 +19,19 @@ spark = (SparkSession.builder.master("local[4]")
 df = spark.read.option("header",True).schema(DatasetSchema().schema).csv(env.getFileName())
 
 # Filtro por origen y destino y por las columnas necesarias
-result_df = df.filter((df.startingAirport == sys.argv[1]) & (df.destinationAirport  == sys.argv[2])).select('flightDate')
+result_df = df.filter((df.startingAirport == sys.argv[1]) & (df.destinationAirport  == sys.argv[2]))
+result_df1 = result_df.select('flightDate', 'segmentsAirlineName')
+
+# Saco la aerolinea
+result_df1 = result_df1.withColumn('Airline', split('segmentsAirlineName', '\|').getItem(0))
 
 # Saco el mes,  YYYY-MM-DD
-result_df1 = result_df.withColumn('month', split('flightDate', '-').getItem(1))
+result_df1 = result_df1.withColumn('Month', split('flightDate', '-').getItem(1))
 
-# Calculo la media de los sitios que sobran por cada hora los redondeo
-result_df2 = result_df1.groupBy('month').count()
+# Calculo el numero de vuelos que hay por cada mes
+result_df2 = result_df1.groupBy('Month', 'Airline').count()
 
 # Ordeno los datos y los escribo en un fichero
-result_df2.sort('month').write.option("header",True).csv("output")
+result_df2.sort('Airline', 'Month').write.option("header",True).csv("FlightsPerMonth")
 
 # spark-submit FlightsPerMonth.py
--- scripts/SeatsRemainingPerHour.py
+++ scripts/SeatsRemainingPerHour.py
@@ -1,4 +1,4 @@
-# Este programa calcula la media de asientos libres de los vuelos con el mismo destino 
+# Este programa calcula la media de asientos libres de los vuelos de cada aerolinea con el mismo destino 
 # y origen pedido. La salida consiste en las horas y la media de asientos.
 
 import sys
@@ -21,17 +21,21 @@ df = spark.read.option("header",True).schema(DatasetSchema().schema).csv(env.get
 
 # Filtro por origen y destino y por las columnas necesarias
 result_df = df.filter((df.startingAirport == sys.argv[1]) & (df.destinationAirport  == sys.argv[2]))
-result_df1 = result_df.select('seatsRemaining','segmentsDepartureTimeRaw')
+result_df1 = result_df.select('seatsRemaining','segmentsDepartureTimeRaw', 'segmentsAirlineName')
+
+# Saco la aerolinea
+result_df1 = result_df1.withColumn('Airline', split(df.segmentsAirlineName, '\|').getItem(0))
 
 # Saco la hora de partida,  YYYY-MM-DDThh:mm:ss.000±[hh]:00
 result_df2 = result_df1.withColumn('auxhour', split(result_df1.segmentsDepartureTimeRaw, 'T').getItem(1))
-result_df3 = result_df2.withColumn('hour', split(result_df2.auxhour, ':').getItem(0)).drop('auxhour','segmentsDepartureTimeRaw')
+result_df3 = result_df2.withColumn('Hour', split(result_df2.auxhour, ':').getItem(0))\
+                       .drop('auxhour','segmentsDepartureTimeRaw', 'segmentsAirlineName')
 
 # Calculo la media de los sitios que sobran por cada hora los redondeo
-result_df3 = result_df3.groupBy('hour').avg('seatsRemaining')
-result_df3 = result_df3.select('hour', floor("avg(seatsRemaining)").alias('media'))
+result_df3 = result_df3.groupBy('Airline', 'Hour').avg('seatsRemaining')
+result_df3 = result_df3.select('Airline','Hour', floor("avg(seatsRemaining)").alias('avgSeats'))
 
 # Ordeno los datos y los escribo en un fichero
-result_df3.sort(result_df3.media.desc()).write.option("header",True).csv("output")
+result_df3.sort('Airline', 'Hour').write.option("header",True).csv("SeatsRemainingPerHour")
 
 # spark-submit SeatsRemainingPerHour.py
