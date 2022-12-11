a16bdd10c7d13ce121f1851e6b7e623917b00dd2
santiest <santiago.rabbia@gmail.com>
2022-12-11 12:23:09 +0100

    Add SparkContext imports

--- scripts/example.py
+++ scripts/example.py
@@ -6,14 +6,18 @@ from schema import DatasetSchema
 
 from pyspark.sql.functions import col
 from pyspark.sql import SparkSession
+from pyspark import SparkFiles
 
-env = EnvVariables()
-print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
 
 # Build pyspark
 spark = (SparkSession.builder.master("local[4]")
             .appName("GetMostVisitedAirportsEXAMPLE")
             .getOrCreate())
+spark.sparkContext.addPyFile(SparkFiles.get("env_wrapper.py"))
+spark.sparkContext.addPyFile(SparkFiles.get("schema.py"))
+
+env = EnvVariables()
+print("\n\n\n\n", env.getFileName(), "\n\n\n\n")
 
 df = spark.read.option("header",True).schema(DatasetSchema().schema).csv(env.getFileName())
 
